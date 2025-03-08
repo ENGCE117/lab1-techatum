09d04f22aa5e14c4e075f9bdb4e1fccde521c0af
Techatum Wongsa <potaeto99@gmail.com>
2025-03-08 18:52:00 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
-void GetMatrix( int **value, int *row, int *col ) ;
-void PrintMatrix( int **value, int row, int col ) ;
+#define MAX_SIZE 100  // กำหนดขนาดสูงสุดของเมทริกซ์
+
+void GetMatrix(int value[MAX_SIZE][MAX_SIZE], int *row, int *col);
+void PrintMatrix(int value[MAX_SIZE][MAX_SIZE], int row, int col);
 
 int main() {
-    int *data, m = 3, n = 3 ;
-    data = new int[ m * n ] ;
-    GetMatrix( &data, &m, &n ) ;
-    PrintMatrix( &data, m, n ) ;
-    return 0 ;
-}//end function
+    int data[MAX_SIZE][MAX_SIZE]; // ใช้อาร์เรย์แบบปกติ ไม่ใช้ malloc
+    int m, n;
 
+    GetMatrix(data, &m, &n);
+    PrintMatrix(data, m, n);
 
-void GetMatrix(int **value, int *row, int *col){
-    scanf("%d", row);
-    scanf("%d", col);
-    for (int i = 0; i < *row; i++)
-    {
-        for (int j = 0; j < *col; j++)
-        {
-            scanf("%d",&value[i * *col + j]);
-        }
-        
-    }
-    
+    return 0;
 }
 
+// ฟังก์ชันรับค่าเมทริกซ์
+void GetMatrix(int value[MAX_SIZE][MAX_SIZE], int *row, int *col) {
+    scanf("%d", row);
+    scanf("%d", col);
 
+    for (int i = 0; i < *row; i++) {
+        for (int j = 0; j < *col; j++) {
+            scanf("%d", &value[i][j]);
+        }
+    }
+}
 
-void PrintMatrix( int **value, int row, int col ) {
-    printf("Matrix (%dx%d):\n", row, col); 
+// ฟังก์ชันแสดงเมทริกซ์
+void PrintMatrix(int value[MAX_SIZE][MAX_SIZE], int row, int col) {
+    printf("Matrix (%dx%d):\n", row, col);
+
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            printf("%d ", value[i * col + j]);
+            printf("%d ", value[i][j]);
         }
         printf("\n");
     }
-    
 }
